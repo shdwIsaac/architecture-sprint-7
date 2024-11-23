2ca7c39a610c4c0531c19ebbd1d8e3f9d48255e8
shdwIsaac <777sam.fisher777@gmail.com>
2024-11-23 15:45:26 +0500

    commit

---
+++ Exc4/binding.sh
@@ -0,0 +1,14 @@
+﻿#!/bin/bash
+# Привязка ролей к пользователям
+
+# Привязка роли cluster-viewer к user1
+kubectl create clusterrolebinding user1-viewer-binding \
+  --clusterrole=cluster-viewer \
+  --user=user1
+
+# Привязка роли cluster-admin к user2
+kubectl create clusterrolebinding user2-admin-binding \
+  --clusterrole=cluster-admin \
+  --user=user2
+
+echo "Роли успешно привязаны к пользователям."
---
+++ Exc4/users.sh
@@ -0,0 +1,17 @@
+﻿#!/bin/bash
+set -e
+
+# Создаем директории для хранения ключей и сертификатов
+mkdir -p certs
+
+# Создание пользователя `user1` (просмотрщик)
+openssl genrsa -out certs/user1.key 2048
+openssl req -new -key certs/user1.key -out certs/user1.csr -subj "/CN=user1/O=cluster-viewer"
+openssl x509 -req -in certs/user1.csr -CA /etc/kubernetes/pki/ca.crt -CAkey /etc/kubernetes/pki/ca.key -CAcreateserial -out certs/user1.crt -days 365
+
+# Создание пользователя `user2` (администратор)
+openssl genrsa -out certs/user2.key 2048
+openssl req -new -key certs/user2.key -out certs/user2.csr -subj "/CN=user2/O=cluster-admin"
+openssl x509 -req -in certs/user2.csr -CA /etc/kubernetes/pki/ca.crt -CAkey /etc/kubernetes/pki/ca.key -CAcreateserial -out certs/user2.crt -days 365
+
+echo "Пользователи user1 и user2 успешно созданы с сертификатами."
