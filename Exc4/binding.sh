54e6f06c2d0bf78f83c22ea5f3faa7b3d37629a1
shdwIsaac <777sam.fisher777@gmail.com>
2024-12-02 19:42:38 +0500

    commit

--- Exc4/binding.sh
+++ Exc4/binding.sh
@@ -5,6 +5,10 @@
 kubectl create clusterrolebinding user1-viewer-binding \
   --clusterrole=cluster-viewer \
   --user=user1
+  
+kubectl create clusterrolebinding user1-secrets-reader-binding \
+  --clusterrole=secrets-reader \
+  --user=user1
 
 # Привязка роли cluster-admin к user2
 kubectl create clusterrolebinding user2-admin-binding \
